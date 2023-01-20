654486917fc1597591b519e2dedf16e8abfa461a
manuh <manuh@LAPTOP-EU21NM8B>
2023-01-20 11:35:38 -0500

    checking

--- src/training/trainingproj.java
+++ src/training/trainingproj.java
@@ -5,7 +5,7 @@ public class trainingproj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("i am in devops");
-
+System.out.println("i am in learning process");
 	}
 
 }
