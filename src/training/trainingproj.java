c8234d9e373440198bdc4f2e0b850eaf4197c81e
manuh <manuh@LAPTOP-EU21NM8B>
2023-01-20 11:31:52 -0500

    first push

---
+++ src/training/trainingproj.java
@@ -0,0 +1,11 @@
+package training;
+
+public class trainingproj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i am in devops");
+
+	}
+
+}
